2c213acbc1d799d3c15c153fb8b36375f857732a
TheBigH50 <HFangmeier@outlook.com>
2023-03-03 12:31:49 -0600

    Initialize project using Generate Express

---
+++ dist/controllers/userController.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getAllUsers = void 0;
+const getAllUsers = (req, res, next) => {
+  res.send('User route');
+};
+exports.getAllUsers = getAllUsers;
