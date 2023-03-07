0356837ca5c3775a89f1118eb87fcd996773d38e
Zach Taylor <zbtaylor1@gmail.com>
2023-03-06 20:22:02 -0500

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,13 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["eslint:recommended", "prettier"],
+  plugins: [],
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
+    ecmaVersion: 2022,
+    sourceType: "module",
   },
-  rules: {
-    'arrow-parens': 0,
-    'comma-dangle': 0,
-    'import/prefer-default-export': 0,
-    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
-    'no-use-before-define': 0,
-    'space-before-function-paren': 0,
-    radix: 0,
+
+  env: {
+    es6: true,
+    node: true,
   },
 };
